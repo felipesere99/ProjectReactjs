4496a35788d06ebd4e983729b818d40f5152a600
Felipe Sere <felipesere99@gmail.com>
2022-08-22 19:04:20 -0300

    Segunda entrega

---
+++ src/components/NavBar.js
@@ -0,0 +1,39 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <>
+    <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+          <img  width="120"
+              height="80"
+              className="d-inline-block align-top" 
+              src="https://tienda.antel.com.uy/razuna/assets/1/C1E491498DD84E71960E4EE38E691C34/img/C7133CA320484B3B9AE5F788FC196A12/antel-fondoazul.png" alt="logo"/>
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Iphone</Nav.Link>
+            <Nav.Link href="#features">Samsung</Nav.Link>
+            <Nav.Link href="#pricing">Otros</Nav.Link>
+          </Nav>
+        </Container>
+        <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Buscar..."
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="primary">Buscar</Button>
+          </Form>
+      </Navbar>
+      </>
+  )
+}
+
+export default NavBar;
